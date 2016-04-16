b55a378d6e3702d214b326f8c28642377fd6b3c2
Fabiano da Rosa Gomes <gomes.fdr@gmail.com>
2016-04-16 16:17:37 -0300

    Removido arquivos sem minificação

---
+++ js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('pesquisaDeLivrosCtrl', function($scope) {
+
+})
+   
+.controller('cadastroDeLivrosCtrl', function($scope) {
+
+})
+   
+.controller('meusLivrosCtrl', function($scope) {
+
+})
+      
+.controller('sobreCtrl', function($scope) {
+
+})
+   
+.controller('resultadoPesquisaCtrl', function($scope) {
+
+})
+   
+.controller('detalhesDoLivroCtrl', function($scope) {
+
+})
+ 
